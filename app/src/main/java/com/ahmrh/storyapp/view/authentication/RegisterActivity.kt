3ec972ee2cfe07af1798f62662591960c1bb5f10
ahmrh <ahmadrolandi01@gmail.com>
2023-05-01 08:44:58 +0700

    adding project

---
+++ app/src/main/java/com/ahmrh/storyapp/view/authentication/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.ahmrh.storyapp.view.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ahmrh.storyapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
